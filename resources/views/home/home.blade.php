556559e7dc887938d4c3d135ce9fe439945947cc
RhaulA <80433033+RhaulA@users.noreply.github.com>
2021-11-13 20:14:21 -0300

    ajuste de layout

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -1,10 +1,8 @@
 @extends('base')
 @section('title') Inicio @endsection
 @section('content')
-    <main class="container-fluid">
         <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-            <h1 class="display-4">ABM de Tareas</h1>
-            <p class="lead">Ejercicio práctico de un ABM completo de un modelo de tarea y categoría.</p>
+            <h1 class="display-4">Dashboard</h1>
         </div>
     </main>
 @endsection
