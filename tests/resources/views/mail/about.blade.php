b6f14079418be1559aa31537d48ee873e876db17
RhaulA <80433033+RhaulA@users.noreply.github.com>
2021-11-19 00:27:16 -0300

    comit actualizado

---
+++ tests/resources/views/mail/about.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <title>Mail</title>
+</head>
+<body>
+    <p>Hola, {{ $about["name"] }}! </p>
+    <p>Tu mensaje fue recibido: {{ $about["message"]}}. Estaremos en contacto pronto..</p>
+</body>
+</html>
+
